21d26256fa6359870489e901b23c8f2b58398694
Kantikumar <kantikumar27@gmail.com>
2019-01-19 14:39:44 +0530

    Final Working HS

--- node.py
+++ node.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3           # This is client.py file
+#!/usr/bin/python3
 import socket
 import threading 
 import time, random
@@ -12,6 +12,8 @@ def server(ID, IP, Port, LeftIP, LeftPort, RightIP, RightPort):
 	replyCount = 0
 	sendMessage = ""
 	sendPort = 0
+
+	'''The Hirschberg–Sinclair algorithm'''
 	while True:
 		data = serverSock.recv(1024)
 		message = data.split(":")
@@ -20,18 +22,12 @@ def server(ID, IP, Port, LeftIP, LeftPort, RightIP, RightPort):
 		elif message[1] == "p" and int(message[2]) > ID and int(message[4]) <= 2**int(message[3]) :
 			sendMessage = str(Port) + ":p:" + message[2] + ":" + message[3] + ":" + str(int(message[4])+1)
 			sendPort = xorPort ^ int(message[0])
-			if sendPort == LeftPort :
-				sendIP = LeftIP
-			else :
-				sendIP = RightIP
+			sendIP = LeftIP if sendPort == LeftPort else RightIP
 			clientSock.sendto(sendMessage, (sendIP, sendPort))
 		elif message[1] == "p" and int(message[2]) > ID and int(message[4]) > 2**int(message[3]) :
 			sendMessage = str(Port) + ":r:" + message[2] + ":" + message[3]
 			sendPort = int(message[0])
-			if sendPort == LeftPort :
-				sendIP = LeftIP
-			else :
-				sendIP = RightIP
+			sendIP = LeftIP if sendPort == LeftPort else RightIP
 			clientSock.sendto(sendMessage, (sendIP, sendPort))
 		elif message[1] == "r" and int(message[2]) != ID :
 			sendMessage = str(Port) + ":r:" + message[2] + ":" + message[3]
@@ -50,93 +46,94 @@ def server(ID, IP, Port, LeftIP, LeftPort, RightIP, RightPort):
 			clientSock.sendto(sendMessage, (LeftIP, LeftPort))
 
 
-
+''' client() function starts the phase 0 processing by sending initial probe messages to both the neighbours'''
 def client(ID, Port, LeftIP, LeftPort, RightIP, RightPort):
 	message = str(Port) + ":p:" + str(ID) + ":" + str(0) + ":" + "1"
 	clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+	'''Sending message to Right Neighbour'''
 	clientSock.sendto(message, (RightIP, RightPort))
+
+	'''Sending message to Left Neighbour'''
 	clientSock.sendto(message, (LeftIP, LeftPort))
-	# clientSock.sendto("hi from " + str(Port), (RightIP, RightPort))
-	# clientSock.sendto("hi from " + str(Port), (LeftIP, LeftPort))
-
-
 
 
 def node_thread(ID, IP, Port, LeftIP, LeftPort, RightIP, RightPort):
+	'''Calling SERVER thread which will keep listening for new messages and process on it'''
 	t1 = threading.Thread(target=server, args=(ID, IP, Port, LeftIP, LeftPort, RightIP, RightPort))
-	t2 = threading.Thread(target=client, args=(ID, Port, LeftIP, LeftPort, RightIP, RightPort))
-	t1.start() 
-	# starting thread 2
-	time.sleep(3) 
-	t2.start()
-	t1.join()
-	t2.join()
 
+	'''Calling CLIENT thread to start the pahse 0'''
+	t2 = threading.Thread(target=client, args=(ID, Port, LeftIP, LeftPort, RightIP, RightPort))
+
+	t1.start()
+	'''Sleeping for 3 seconds so that all the other thread's Server can start'''
+	time.sleep(3)
+
+	t2.start()
+	t2.join()
+	t1.join()
 
 
 def main():
 	number = raw_input("Enter number of nodes:")
 	number_nodes = int(number)
-	InitIp = "127.0.0."
+	Sec_suffix = 0
+	First_suffix = 1
+	InitIp = "127.0"
 	InitPort = 5000
-	UID = random.sample(range(1, 1000), number_nodes)
+	lastIP_first_suffix = number_nodes % 254
+	lastIP_sec_suffix = number_nodes / 254
+	LastIP = InitIp + "." + str(lastIP_sec_suffix) + "." + str(lastIP_first_suffix)
+
+	'''Generating random unique ID for processes'''
+	UID = random.sample(range(1, 10000), number_nodes)
 	for i in UID:
 		print i
 	print "Elected leader should be: " , max(UID)
+
+	'''Assigning Left neighbour and Right neighbour to a node'''
 	for i in range(number_nodes):
-		IP = InitIp + str(i+1)
+		if First_suffix%255 == 0 and First_suffix != 0:
+			Sec_suffix = Sec_suffix+1
+			First_suffix = 1
+		IP = InitIp + "." + str(Sec_suffix) + "." + str(First_suffix)  
+
 		Port = InitPort + i
-		ip_ind = i
 		port_ind = i-1
 		if i-1 < 0:
-			ip_ind = number_nodes
 			port_ind = number_nodes - 1
-		LeftIP = InitIp + str(ip_ind)
 		LeftPort = InitPort + port_ind
+		if First_suffix == 1 :
+			if Sec_suffix-1 >= 0 :
+				LeftIP = InitIp + "." + str(Sec_suffix-1) + ".254"
+			else:
+				LeftIP = LastIP
+		else:
+			LeftIP = InitIp + "." + str(Sec_suffix) + "." + str(First_suffix-1)
 
 		ip_ind1 = i
 		port_ind1 = i-1
 		if i+1 == number_nodes:
-			ip_ind1 = -1
 			port_ind1 = -2
-		RightIP = InitIp + str(ip_ind1+2)
 		RightPort = InitPort + port_ind1+2
-		print IP + " " + str(Port) + " " + LeftIP + " " +  str(LeftPort) + " "  + RightIP + " "  + str(RightPort)
+
+		if First_suffix == 254:
+			if Sec_suffix < lastIP_sec_suffix:
+				RightIP = InitIp +"." + str(Sec_suffix+1) + ".1"
+			else:
+				RightIP = "127.0.0.1"
+
+		elif IP == LastIP:
+			RightIP = "127.0.0.1"
+
+		else:
+			RightIP = InitIp + "." + str(Sec_suffix) + "." + str(First_suffix+1)
+
+		First_suffix = First_suffix + 1
+		#print IP + " " + str(Port) + " " + LeftIP + " " +  str(LeftPort) + " "  + RightIP + " "  + str(RightPort)
+
+		'''Creating a node as a seperate thread and passing ID, IP, Port, Left neighbour and Right neighbour'''
 		threading.Thread(target=node_thread, args=(UID[i], IP, Port, LeftIP, LeftPort, RightIP, RightPort)).start() 
-
-
-
-
-  
-
-'''Class Node: 
-	def __init__(self, NodeId, NodeHost, NodePort, clockwiseNeighbor, antiClockwiseNeighbor): 
-		self.NodeId=NodeId 
-		self.IP=NodeHost 
-		self.port=NodePort 
-		self.clockwiseNeighbor=clockwiseNeighbor 
-		self.antiClockwiseNeighbor=antiClockwiseNeighbor 
-  
-
-	def joinRing(self, rightnode): #raise RingException 
-		# create a socket object
-		serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		# bind to the port
-		serversocket.bind((self.IP, self.port))                                  
-		# queue up to 5 requests
-		serversocket.listen(5)                                           
-
-		while True:
-   			# establish a connection
-   			clientsocket,addr = serversocket.accept()
-
-   		#create client socket
-   		clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-
-	def sendMessageClockwise(self, message): #raise RingException  
-
-	def sendMessageCounterClockwise(self, message) #raise RingException '''
 
 
 if __name__== "__main__":
